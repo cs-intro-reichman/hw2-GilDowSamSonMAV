25c7ef7d9cd28a3780bae86b6a92a2d12b18591c
gil dow <gil.dow@runi.ac.il>
2025-11-09 16:42:24 +0200

    NewSt Version22

--- Cheers.java
+++ Cheers.java
@@ -7,28 +7,26 @@ public class Cheers {
                 String s = "AEFHILMNORSX";
 
                 while (i < R.length()) {
-                                char x = R.charAt(i);
-                                char upper = Character.toUpperCase(x);  
-                                if (s.indexOf(upper) == -1) {
+                        char x = R.charAt(i);
+                        char upper = Character.toUpperCase(x);
+                        if (s.indexOf(upper) == -1) {
 
-                                        System.out.println("Give me a  " + upper + ": " + upper + "!");
-                                } else {
+                                System.out.println("Give me a  " + upper + ": " + upper + "!");
+                        } else {
 
-                                        System.out.println("Give me an " + upper + ": " + upper + "!");
-                                }
-                                i++;
+                                System.out.println("Give me an " + upper + ": " + upper + "!");
+                        }
+                        i++;
 
                 }
                 System.out.println("What does that spell?");
 
-                int q = 0;
+             
 
-                do {
-
-                        System.out.println(R.toUpperCase() + "!!!");
-
-                        q++;
-                } while (q < f);
+                for (int q = 0 ; q < f ; q++) {
+                        System.out.println(R + "!!!");
+                    
+                }
         }
 
 }
